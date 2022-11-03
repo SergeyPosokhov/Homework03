88a3f37adbdb52ce1bf189f7bcb4cfbbd784739f
Sergey Posokhov <posokhov_pit@mail.ru>
2022-11-03 12:24:57 +0300

    feat: add Homework03 Task 023

---
+++ Homework03_Task023/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 23
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+Console.WriteLine("Введите какое-нибудь число ");
+int num = Convert.ToInt32(Console.ReadLine());
+int i = 1;
+while (i < num)
+{
+Console.Write(i * i * i + ", ");
+i = i + 1;
+}
+if (i == num)
+{
+    Console.WriteLine(i * i * i + "");
+}
